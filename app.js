14e474a29c9abb4b4132fe4a367d17fb4c4e68fe
Cory Tveten <corytveten@gmail.com>
2021-09-11 21:29:53 -0400

    building out fetch

--- app.js
+++ app.js
@@ -17,5 +17,47 @@ const APP = {
         nav.addEventListener('click', APP.getData);
         nav.addEventListener('click', APP.getData);
     },
-    
+    buildNav: () => {
+        let df = new DocumentFragment();
+        for (let nm in APP.urls) {
+            if (nm != 'base') {
+                let link = document.createElement('a');
+                link.href = `${APP.urls.base}${APP.urls[nm]}`;
+                link.textContent = nm;
+                link.setAttribute('data-link', `${APP.urls.base}${APP.urls[nm]}`)
+                df.append(link);
+            }
+        }
+        document.getElementById('nav').append(df);
+    },
+    getData: (ev) => {
+        if (ev) ev.preventDefault();
+
+        let link = ev.target;
+        let url = link.getAttribute('data-link');
+
+        fetch(url)
+            .then(resp => {
+
+            })
+            .then(App.buildList)
+            .catch(console.error)
+    },
+    buildList: (data) = {
+        let m = document.getElementById('main');
+        console.log(data);
+
+        let footer = document.getElementById('footer');
+        footer.innerHTML = '';
+
+        if (data.previous) {
+
+        }
+        if (data.next) {
+
+        }
+    }
+
 }
+
+document.addEventListener('DOMContentLoaded', APP.init)
